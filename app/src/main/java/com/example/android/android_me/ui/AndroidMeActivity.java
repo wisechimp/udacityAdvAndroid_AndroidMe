67a58517baa6c53541438da513423a26c5cc80c1
Stuart Hicks <stuart.hicks@gmail.com>
2019-01-07 13:43:14 +0100

    More initialising
    Adding all the "initial" files that weren't included in the initial clone!

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,13 +1,15 @@
-package com.example.android.android_me;
+package com.example.android.android_me.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.android_me.R;
+
+public class AndroidMeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_android_me);
     }
 }
